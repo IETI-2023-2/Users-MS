8913c70c8d1ffcde6159520f0fbb3a86f581a2e1
Santiago Bayona <santiago.bayona@mail.escuelaing.edu.co>
2023-09-19 19:16:11 -0500

    first commit

---
+++ src/main/java/edu/escuelaing/UsersMSApplication.java
@@ -0,0 +1,13 @@
+package edu.escuelaing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersMSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersMSApplication.class, args);
+	}
+
+}
